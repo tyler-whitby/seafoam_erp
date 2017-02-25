99ad91a8b91f9249f3b6d52099e2a7787c09ecac
Tyler Kontra <tkontra551@gmail.com>
2017-02-24 17:12:16 -0800

    started to theme

--- clearwave/settings.py
+++ clearwave/settings.py
@@ -215,6 +215,7 @@ STATIC_URL = "/site_media/static/"
 # Additional locations of static files
 STATICFILES_DIRS = [
     os.path.join(PROJECT_ROOT, "static", "dist"),
+    os.path.join(PROJECT_ROOT, "clearwave", "static"),
 ]
 
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
