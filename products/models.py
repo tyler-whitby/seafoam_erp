349ad144d024fbb247e2ab351fbd2f60d1d08ec7
Tyler Kontra <tkontra551@gmail.com>
2017-02-25 17:01:27 -0800

    initial product, category models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=256,primary_key=True,unique=True)
+    product_category = models.ForeignKey(Category)
+    #product_subcategory = models.ForeignKey(Subcategory)
+
+class Category(models.Model):
+    parent = models.ForeignKey('self', null=True, blank=True)
+    category_name = models.CharField(max_length=64, primary_key=True)
+
+'''
+Implement later......
+
+class Subcategory(models.Model):
+    parent = models.ForeignKey(Category)
+    name = models.CharField(max_length=64)
+
+'''
