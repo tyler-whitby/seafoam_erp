56c281cfc19588f551daa060a0295c837a74e1fa
Tyler Kontra <tkontra551@gmail.com>
2017-02-26 19:39:28 -0800

    static back to site_media

--- clearwave/settings.py
+++ clearwave/settings.py
@@ -213,7 +213,7 @@ STATIC_ROOT = os.path.join(PACKAGE_ROOT, "site_media", "static")
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = "static/"
+STATIC_URL = "/site_media/static/"
 
 # Additional locations of static files
 STATICFILES_DIRS = [
